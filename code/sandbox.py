a34f02794f557e6643f4de255e09380416f60d06
1kastner <1kastner@informatik.uni-hamburg.de>
2015-12-27 01:58:08 +0200

    Adjust sandbox.py to conventions.

--- code/sandbox.py
+++ code/sandbox.py
@@ -3,6 +3,7 @@
 import os
 import bob.bio.spear.preprocessor.Base
 
+
 def get_base_preprocessed_file(file_name):
     """
     @type file_name: String
@@ -15,15 +16,14 @@ def get_base_preprocessed_file(file_name):
     audio_file_path = os.path.join(project_base_dir, "AudioFiles", file_name)
     base_preprocessor = bob.bio.spear.preprocessor.Base()
     return base_preprocessor.read_original_data(audio_file_path)
-    #import pkg_resources
-    #return base_preprocessor.read_original_data(pkg_resources.resource_filename('bob.bio.spear.test', 'data/sample.wav'))
+
 
 def extract_mfcc_60(base_preprocessed_data):
     """
-    @type base_preprocessed_file:  (<Hertz>, <numpy array>)
+    @type base_preprocessed_data:  (<Hertz>, <numpy array>)
     """    
     preprocessor = bob.bio.spear.preprocessor.Energy_2Gauss()
-    #@type preprocessed_data: (<Herz>, <numpyArray>, <speaker vs. silence>)
+    # @type preprocessed_data: (<Hertz>, <numpyArray>, <speaker vs. silence>)
     preprocessed_data = preprocessor(base_preprocessed_data)
     
     extractor = bob.bio.base.load_resource('mfcc-60', 'extractor')
@@ -33,5 +33,3 @@ def extract_mfcc_60(base_preprocessed_data):
 if __name__ == "__main__":
     base_pre_data = get_base_preprocessed_file("firat_speakerRecognitionWiki_16khz_mono.wav")
     print extract_mfcc_60(base_pre_data)
-
-    
