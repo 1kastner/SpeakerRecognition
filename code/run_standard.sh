63315aca6284856b4040ac660191c17c6e602e9b
1kastner <1kastner@informatik.uni-hamburg.de>
2016-01-01 20:55:53 +0200

    Quickfix #2.

--- code/run_standard.sh
+++ code/run_standard.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
+
+# set these to your local environment
 alias python='/home/marvin/Downloads/bob-2.0.6/bin/python'
 HOME=/home/marvin
 ARFF_TARGET="$HOME/projectrepository/results"
+MAIN=$HOME/projectrepository/code/main.py
+
+# start
+
 python $HOME/projectrepository/code/main.py HEADER > "$ARFF_TARGET/header.tmp"
 HEADER="$ARFF_TARGET/header.tmp"
-MAIN=$HOME/projectrepository/code/main.py
 
 Wiki () {
     python $MAIN $1 $2.wav > $2"Data.arff"
-    cat $HEADER "$ARFF_TARGET"/"$2Data.arff" > "$ARFF_TARGET"/"$2Wiki.arff"
+    cat "$HEADER" "$ARFF_TARGET/$2Data.arff" > "$ARFF_TARGET/$2Wiki.arff"
 }
 
 Wiki Firat FiratWiki
 Wiki Marvin MarvinWiki
 Wiki Kamuran KamuranWiki
 
-cat HEADER "$ARFF_TARGET/MarvinWikiData.arff" "$ARFF_TARGET/KamuranWikiData.arff" "$ARFF_TARGET/FiratWikiData.arff" > "$ARFF_TARGET/AllWiki.arff"
+cat "$HEADER" "$ARFF_TARGET/MarvinWikiData.arff" "$ARFF_TARGET/KamuranWikiData.arff" "$ARFF_TARGET/FiratWikiData.arff" > "$ARFF_TARGET/AllWiki.arff"
