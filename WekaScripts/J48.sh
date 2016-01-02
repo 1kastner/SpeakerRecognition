7f08b3d3e35f4e5e974d9842e291974d99c1e615
1kastner <1kastner@informatik.uni-hamburg.de>
2016-01-02 15:54:14 +0200

    Modify J48 for Firat.

--- WekaScripts/J48.sh
+++ WekaScripts/J48.sh
@@ -5,20 +5,25 @@ PROJECTREPOSITORY="/home/marvin/projectrepository"
 
 RunAlg () {
     CONFIDENCE="-C "${1}
-    if ${2}; then
+    if [ ${2}=S ]; then
         OPTS="-"${2}
     fi
     echo run weka -c "weka.classifiers.trees.J48 -t ${PROJECTREPOSITORY}/results/all.arff -x 2 ${OPTS} ${CONFIDENCE} ${OPTS}"
-    weka -m2G -c "weka.classifiers.trees.J48 -t ${PROJECTREPOSITORY}/results/all.arff -x 2 ${OPTS} ${CONFIDENCE}" > "${PROJECTREPOSITORY}/WekaResults/J48-C{1}${2}.txt"
+    weka -m2G -c "weka.classifiers.trees.J48 -t ${PROJECTREPOSITORY}/results/all.arff -x 2 ${OPTS} ${CONFIDENCE}" > "${PROJECTREPOSITORY}/WekaResults/J48-C${1}${2}.txt"
 }
 
 RunAllJ48() {
-    for confidence in 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5
+    #for confidence in 0.1 0.15 0.2 
+    #do
+    #    RunAlg $confidence S
+    #done
+
+    for confidence in 0.35 0.4 0.45 0.5
     do
         RunAlg $confidence
         RunAlg $confidence S
     done
+
 }
 
-#RunAlg 0.1
 RunAllJ48
