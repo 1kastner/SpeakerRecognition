6c0a3771989caec067b147d3e8c7db4fb16bbcc2
1kastner <1kastner@informatik.uni-hamburg.de>
2016-01-02 13:08:46 +0200

    Add J48 script for result creation.

--- WekaScripts/J48.sh
+++ WekaScripts/J48.sh
@@ -5,17 +5,20 @@ PROJECTREPOSITORY="/home/marvin/projectrepository"
 
 RunAlg () {
     CONFIDENCE="-C "${1}
-    OPTS=${2}
+    if [${2}]; then
+        OPTS="-"${2}
+    fi
     echo run weka -c "weka.classifiers.trees.J48 -t ${PROJECTREPOSITORY}/results/all.arff -x 2 ${OPTS} ${CONFIDENCE}"
-    weka -c "weka.classifiers.trees.J48 -t ${PROJECTREPOSITORY}/results/all.arff -x 2 ${OPTS} ${CONFIDENCE}" > "${PROJECTREPOSITORY}/WekaResults/J48${CONFIDENCE}${OPTS}.txt"
+    weka -m2G -c "weka.classifiers.trees.J48 -t ${PROJECTREPOSITORY}/results/all.arff -x 2 ${OPTS} ${CONFIDENCE}" > "${PROJECTREPOSITORY}/WekaResults/J48-C{1}${2}.txt"
 }
 
 RunAllJ48() {
     for confidence in 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5
     do
         RunAlg $confidence
-        RunAlg $confidence -S
+        RunAlg $confidence S
     done
 }
 
-RunAlg 0.1
+#RunAlg 0.1
+RunAllJ48
