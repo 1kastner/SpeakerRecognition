3d367d31ab14e725485dc797a7c0d02bb4b9c636
1kastner <1kastner@informatik.uni-hamburg.de>
2016-01-03 15:15:58 +0200

    add tsvs and plots

--- Plots/plotter.py
+++ Plots/plotter.py
@@ -25,6 +25,7 @@ def compare_j48_subtree_raising():
     pyplot.grid(True)
     pyplot.show() 
 
+
 def compare_confidence_without_subtree_raising():
     pyplot.ylabel('accuracy')
     pyplot.xlabel('confidence')
@@ -38,6 +39,7 @@ def compare_confidence_without_subtree_raising():
     pyplot.grid(True)
     pyplot.show() 
 
+
 def compare_one_rule():
     pyplot.ylabel('accuracy')
     pyplot.xlabel('bucket size')
@@ -50,7 +52,57 @@ def compare_one_rule():
             g = pyplot.plot(float(row[0]), float(row[1]), "bs")
     pyplot.grid(True)
     pyplot.show() 
+
+
+def compare_random_forest():
+    pyplot.ylabel('accuracy')
+    pyplot.xlabel('forest size')
+    with open("RandomForest.tsv", "r") as o:
+        reader = csv.reader(o, delimiter="\t")
+        for row in reader:
+            break # discard for line
+        for row in reader:
+            print row[0], row[1]
+            g = pyplot.plot(float(row[0]), float(row[1]), "bs")
+    pyplot.grid(True)
+    pyplot.show() 
+
+
+def compare_single_layer_anns():
+    pyplot.ylabel('accuracy')
+    pyplot.xlabel('number of epoches')
+    with open("SingleLayerMultiLayerPerceptron.tsv", "r") as o:
+        reader = csv.reader(o, delimiter="\t")
+        for row in reader:
+            break # discard for line
+        for row in reader:
+            if row[0] == "0.1":
+                pyplot.plot(float(row[1]), float(row[2]), "bs")
+            elif row[0] == "0.2":
+                pyplot.plot(float(row[1]), float(row[2]), "g^")
+            elif row[0] == "0.3":
+                pyplot.plot(float(row[1]), float(row[2]), "r+")                
+    pyplot.grid(True)
+    pyplot.show() 
+      
+      
+def check_alpha_0_1_converging():
+    pyplot.ylabel('accuracy')
+    pyplot.xlabel('number of epoches')
+    with open("SingleLayerMultiLayerPerceptron.tsv", "r") as o:
+        reader = csv.reader(o, delimiter="\t")
+        for row in reader:
+            break # discard for line
+        for row in reader:
+            if row[0] == "0.1":
+                pyplot.plot(float(row[1]), float(row[2]), "bs")             
+    pyplot.grid(True)
+    pyplot.show() 
+    
     
 #compare_j48_subtree_raising()
 #compare_confidence_without_subtree_raising()
-compare_one_rule()
+#compare_one_rule()
+#compare_random_forest()
+#compare_single_layer_anns()
+check_alpha_0_1_converging()
